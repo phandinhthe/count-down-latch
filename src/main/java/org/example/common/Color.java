cc0a0e5484aa889c38cf83ce25f8ee55f80183f4
phandinhthe <phandinhthe1991@gmail.com>
2024-05-11 00:02:17 +0700

    Implement a worker and report apply Callable and CountDownLatch.

---
+++ src/main/java/org/example/common/Color.java
@@ -0,0 +1,21 @@
+package org.example.common;
+
+public enum Color {
+	BLACK("\u001B[0m"),
+	ORANGE("\u001B[38;5;208m"),
+	RED("\u001B[31m"),
+	GREEN("\u001B[32m"),
+	YELLOW("\u001B[38;5;11m"),
+	MAGENTA("\u001B[38;5;201m"),
+	VIOLET("\u001B[38;5;093m");
+
+	private final String encodedColor;
+
+	Color(String encodedColor) {
+		this.encodedColor = encodedColor;
+	}
+
+	public String encodedColor() {
+		return encodedColor;
+	}
+}
